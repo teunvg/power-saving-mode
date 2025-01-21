10e2c75d1ea063645fb7ef1b093fdb00050afefe
Joris Vincent <joris.vincent@tu-berlin.de>
2025-01-21 21:28:16 +0100

    vote analysis more clearly by state

--- votes.R
+++ votes.R
@@ -1,19 +1,17 @@
 library(tidyverse)
 
-df <- read_csv("data/vote_preferences_by_division.csv", skip=1)
-
-vote_counts <- df |>
+df <- read_csv("data/vote_preferences_by_division.csv", skip=1) |>
   pivot_wider(names_from = CalculationType, values_from = CalculationValue) |>
-  rename(c("preference_count" = `Preference Count`)) |>
+  rename(c("preference_count" = `Preference Count`))
+
+state_counts <- df |>
   group_by(StateAb, PartyNm) |>
   summarise(total_count = sum(preference_count)) |>
   arrange(StateAb, -total_count)
-  
-vote_counts <- vote_counts |>
   group_by(StateAb) |>
   mutate(relative_count = total_count / sum(total_count))
 
-vote_counts |> 
+state_counts |> 
   filter(relative_count >= 0.10) |>
   ungroup() |>
   distinct(PartyNm) |>
