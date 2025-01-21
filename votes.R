ab89ba2e46177e890cc8e6f1858b46b2efd93a5c
Joris Vincent <joris.vincent@tu-berlin.de>
2025-01-21 21:03:47 +0100

    vote counts and proportions, per state

---
+++ votes.R
@@ -0,0 +1,14 @@
+library(tidyverse)
+
+df <- read_csv("data/vote_preferences_by_division.csv", skip=1)
+
+vote_counts <- df |>
+  pivot_wider(names_from = CalculationType, values_from = CalculationValue) |>
+  rename(c("preference_count" = `Preference Count`)) |>
+  group_by(StateAb, PartyNm) |>
+  summarise(total_count = sum(preference_count)) |>
+  arrange(StateAb, -total_count)
+  
+vote_counts <- vote_counts |>
+  group_by(StateAb) |>
+  mutate(relative_count = total_count / sum(total_count))
