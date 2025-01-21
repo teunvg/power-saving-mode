51cb032bec84f412d657a4072f0c58bc1ef90e7c
Joris Vincent <joris.vincent@tu-berlin.de>
2025-01-21 22:26:06 +0100

    leanings per polling place

--- votes.R
+++ votes.R
@@ -1,23 +1,9 @@
 library(tidyverse)
 
-df <- read_csv("data/vote_preferences_by_division.csv", skip=1) |>
-  pivot_wider(names_from = CalculationType, values_from = CalculationValue) |>
-  rename(c("preference_count" = `Preference Count`))
+## Define states
+states = c("ACT","NSW","NT","QLD","SA","TAS", "VIC","WA")
 
-state_counts <- df |>
-  group_by(StateAb, PartyNm) |>
-  summarise(total_count = sum(preference_count)) |>
-  arrange(StateAb, -total_count) |>
-  group_by(StateAb) |>
-  mutate(relative_count = total_count / sum(total_count))
-
-state_counts |> 
-  filter(relative_count >= 0.10) |>
-  ungroup() |>
-  distinct(PartyNm) |>
-  write_csv("data/major_parties_list.csv")
-
-###-- party import and cleaning
+## Collapse same parties with "different" names
 parties_major <- read_csv('data/party-merge') |>
   rename(PartyNm = Party_Was, Party=Party_Be)
 
@@ -25,20 +11,28 @@ parties_leaning <- read_csv('data/party-leanings') |>
   rename(p_or_c=`p-or-c`, l_or_r=`l-or-r`) |>
   filter(!is.na(l_or_r))
 
-state_leanings <- state_counts |>
-  left_join(parties_major) |>
-  left_join(parties_leaning) %>%
-  mutate(
-    p_or_c = ifelse(p_or_c < 0, 'p', 'c'),
-    l_or_r = ifelse(l_or_r < 0, 'l', 'r'),
-    leaning = ifelse(p_or_c=='p', 'Progressive/Left', 'Conservative/Right')
-  ) |>
-  select(-PartyNm, -p_or_c, -l_or_r) %>%
-  mutate(Party = ifelse(Party == 'Independent', NA, Party)) |>
-  group_by(StateAb, Party, leaning) %>%
-  summarize(
-    total_count = sum(total_count),
-    relative_count = sum(relative_count)
-  )
 
-state_leanings |> write_csv('data/state_leanings.csv')
+## Load polling places
+pps <- read_csv("data/pps_ACT.csv", skip=1)
+for (state in states[-1]) {
+  pps <- bind_rows(pps, read_csv(str_glue("data/pps_", state, ".csv"), skip=1))
+}
+pps <- pps |>
+  select(StateAb, DivisionID, DivisionNm, PollingPlaceID, PollingPlace, PartyNm, OrdinaryVotes)
+
+## Clean party names
+pps <- pps |>
+  left_join(parties_major) |>
+  select(-PartyNm) |>
+  drop_na()
+
+pp_leanings <- pps |> 
+  group_by(StateAb, DivisionID, DivisionNm, PollingPlaceID, PollingPlace, Leaning) |>
+  summarise(OrdinaryVotes = sum(OrdinaryVotes)) |>
+  group_by(StateAb, DivisionID, DivisionNm, PollingPlaceID, PollingPlace) |>
+  mutate(ppt_votes = OrdinaryVotes / sum(OrdinaryVotes)) |>
+  select(-OrdinaryVotes) |>
+  pivot_wider(names_from=Leaning, values_from=ppt_votes) |>
+  mutate(leaning = Conservative - Progressive)
+
+pp_leanings |> write_csv('data/pp_leanings.csv')
