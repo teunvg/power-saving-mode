b9d8ef932bafa507236d358d2fed98a25aea7a5d
Joris Vincent <joris.vincent@tu-berlin.de>
2025-01-22 00:00:04 +0100

    more detailed toilets with leanings

--- toilet_leanings.R
+++ toilet_leanings.R
@@ -1,10 +1,17 @@
 library(tidyverse)
 
+# Read necessary data
 toilets <- read_csv("data/toilets.csv")
 toilet_mappings <- read_csv("data/toilet_polling_station_mapping.csv")
 pp_leanings <- read_csv('data/pp_leanings.csv')
 
+# Map toilets, to polling places, to political leanings
 toilet_leanings <- toilets |>
   left_join(toilet_mappings) |>
   left_join(pp_leanings) |>
-  select(toilet = Name, Address1, Town, State, leaning)
+  select(Toilet = Name, Leaning=leaning, ToiletAddress = Address1, PollingPlace, everything()) |>
+  select(-FacilityID)
+
+# Write combined data to file
+toilet_leanings |> write_csv("data/toilet_leanings.csv")
+
